c4a6629711050ccb74323771152c659b0b3c4f7f
Denis Kolyasev <kolyasev@gmail.com>
2012-12-07 23:22:13 +0400

    menu item for purchasing agent added

--- Modules/Purchase/Purchase.php
+++ Modules/Purchase/Purchase.php
@@ -1,3 +1,44 @@
 <?php
 
-$view['Content'] = 'Templates/Purchase/Purchase.php';
+require_once __DIR__.'/../StoreDB/StoreDB.php';
+
+$purchase = new Purchase();
+$purchase->init();
+$view = $purchase->view;
+
+class Purchase {
+
+    public $view;
+
+    /* @var StoreDB */
+    public $db;
+
+    public function init() {
+        if (!$this->check_permissions()) {
+            $this->error_page('Чтобы иметь доступ к этой странице вы должны иметь роль закупщика склада!');
+            return;
+        }
+
+        if (isset($_SESSION['companyID']) && $_SESSION['companyID']) {
+            $this->db = new StoreDB($_SESSION['companyID']);
+        } else {
+            $this->error_page('Произошла ошибка...');
+            return;
+        }
+
+        $this->view['Content'] = 'Templates/Purchase/Purchase.php';
+    }
+
+    private function check_permissions() {
+        if ($_SESSION['rank'] == 'Закупщик магазина') {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function error_page($text) {
+        $this->view['error'] = $text;
+        $this->view['Content'] = 'Templates/Purchase/ErrorPage.php';
+    }
+}
--- Templates/Menu.php
+++ Templates/Menu.php
@@ -13,23 +13,30 @@
                 <li class="header_menu_item">
                 <a href='/Settings'>Настройки</a></li>
 				<li class="menuSeparator"></li>
-            <?php } ?>  
-
-            <!--Administration-->
-            <?php if($_SESSION['connected'] && $_SESSION['rank'] == 'Администратор') { ?>
-            <li class="header_menu_item"><a href="#">
-                <a href="/Administration">Администрация</a>
-                <nav class="header_subMenu">
-                <ul>
-                    <li class="menuSeparator"></li>
-                    <li class="heder_subMenu_item"><a href="/Administration/Staff">Управления соотрудников</a></li>
-                    <li class="menuSeparator"></li>
-                    <li class="heder_subMenu_item"><a href="/Administration/WorkPlaces">Управления пунктами</a></li>
-                    <li class="menuSeparator"></li>
-                </ul>
-                </nav></li>
-                <li class="menuSeparator"></li>
             <?php } ?>
+
+                <!--Administration-->
+                <?php if($_SESSION['connected'] && $_SESSION['rank'] == 'Администратор') { ?>
+                <li class="header_menu_item"><a href="#">
+                    <a href="/Administration">Администрация</a>
+                    <nav class="header_subMenu">
+                        <ul>
+                            <li class="menuSeparator"></li>
+                            <li class="heder_subMenu_item"><a href="/Administration/Staff">Управления соотрудников</a></li>
+                            <li class="menuSeparator"></li>
+                            <li class="heder_subMenu_item"><a href="/Administration/WorkPlaces">Управления пунктами</a></li>
+                            <li class="menuSeparator"></li>
+                        </ul>
+                    </nav></li>
+                <li class="menuSeparator"></li>
+                <?php } ?>
+
+                <!--Закупщик магазина-->
+                <?php if($_SESSION['connected'] && $_SESSION['rank'] == 'Закупщик магазина') { ?>
+                <li class="header_menu_item"><a href="#">
+                    <a href="/Purchase">Закупщик магазина</a></li>
+                <li class="menuSeparator"></li>
+                <?php } ?>
         </ul>
     </nav>
 
