f1c8e8d256c97a1d9af74e03a79120b6e0bbc660
Ikarium <eva.axis@gmail.com>
2012-12-15 19:31:42 +0400

    Revert "Menu correction/adaptation"
    This reverts commit 17ebebd08788d97fd85e8abd51da746abd2f6ef8.

--- Templates/Menu.php
+++ Templates/Menu.php
@@ -16,27 +16,20 @@
             <?php } ?>
 
                 <!--Administration-->
-                <?php
-				
-                if($_SESSION['connected']){
-                    $rank = array();
-                    foreach($_SESSION['post'] as $post) $ranks[] = $post['rank'];
-                    if(in_array('Администратор', $ranks)) { ?>
-
-                        <li class="header_menu_item">
-                            <a href="/Administration">Администрация</a>
-                            <nav class="header_subMenu">
-                            <ul>
-                                <li class="menuSeparator"></li>
-                                <li class="heder_subMenu_item"><a href="/Administration/Staff">Управления соотрудников</a></li>
-                                <li class="menuSeparator"></li>
-                                <li class="heder_subMenu_item"><a href="/Administration/WorkPlaces">Управления пунктами</a></li>
-                                <li class="menuSeparator"></li>
-                            </ul>
-                            </nav></li>
+                <?php if($_SESSION['connected'] && $_SESSION['rank'] == 'Администратор') { ?>
+                <li class="header_menu_item"><a href="#">
+                    <a href="/Administration">Администрация</a>
+                    <nav class="header_subMenu">
+                        <ul>
                             <li class="menuSeparator"></li>
-
-                    <?php } ?>
+                            <li class="heder_subMenu_item"><a href="/Administration/Staff">Управления соотрудников</a></li>
+                            <li class="menuSeparator"></li>
+                            <li class="heder_subMenu_item"><a href="/Administration/WorkPlaces">Управления пунктами</a></li>
+                            <li class="menuSeparator"></li>
+                        </ul>
+                    </nav></li>
+                <li class="menuSeparator"></li>
+                <?php } ?>
 
                     <li class="header_menu_item">
                         Рабочея место
@@ -56,7 +49,6 @@
                             </ul>
                         </nav></li>
                     <li class="menuSeparator"></li> 
-                <?php } ?>
 
                 <!--Закупщик магазина-->
                 <?php if($_SESSION['connected'] && $_SESSION['rank'] == 'Закупщик магазина') { ?>
