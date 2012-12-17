1455d21894d6f631d036157a83b83f343603d627
Maxim Rudik <roninmax12@gmail.com>
2012-12-18 01:06:36 +0700

    Update Templates/Expert/Expert.php

--- Templates/Expert/Expert.php
+++ Templates/Expert/Expert.php
@@ -18,7 +18,7 @@
             <td><?=$item->name?></td>
             <td><?=$item->ware_count?></td>
             <td><?=$item->sold_count?></td>
-            <td><input type="text" name="item[<?=$item->itemID?>]" value="0" onmouseout="update(this.name);"></td>
+            <td><input type="text" name="item[<?=$item->itemID?>]" value="0" onBlur="update(this.name);"></td>
             <td><span class="result[<?=$item->itemID?>]"><?=$item->result?></span></td>
             
         </tr>
@@ -29,18 +29,27 @@
 <?php endif; ?>
 
 <script type="text/javascript">
+var prev_value = 0;
+var modified = false;
 
 var update = function(th){
     var elem_val = document.getElementsByName(th)[0].value;
     if (elem_val != "Nan"){
         var res_name = 'result' + th.substr(4);
-        var result_obj = document.getElementsByClassName(res_name)[0];
-        var result = parseInt(elem_val) + parseInt(result_obj.innerHTML);
-            result_obj.innerHTML = result;
-    }
-    else{
-        var result_obj = document.getElementsByClassName(res_name)[0];
-            result_obj.innerHTML = '0';
+        var result_obj = document.getElementsByClassName(res_name)[0];        
+        if (!modified)
+        {
+            prev_value = parseInt(elem_val);
+            var result = parseInt(elem_val) + parseInt(result_obj.innerHTML);
+                result_obj.innerHTML = result;
+            modified = true;
+        }
+        else
+        {
+            var result = parseInt(elem_val) + parseInt(result_obj.innerHTML) - prev_value;
+            modified = false;
+            prev_value = 0;
+        }
     }
 }
 
